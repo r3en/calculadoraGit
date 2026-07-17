573d167a7ab483127da6d94896a7518cc6658196
r3en <helenarrioja@gmail.com>
2026-07-17 15:43:12 -0700

    sepa la verga tmb unas pinches multiplicaciones ya me enrede

--- calculadora.py
+++ calculadora.py
@@ -4,5 +4,9 @@ def suma(a, b):
 def resta(a, b):
     return a - b
 
+def multiplicacion(a, b):
+    return a * b
+
 print("Suma:", suma(5, 3))
 print("Resta:", resta(5, 3))
+print("Multiplicación:", multiplicacion(5, 3))
